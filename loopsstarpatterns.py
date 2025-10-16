ac3beade16d2aa20cd1e9fc264d891c502dbf15d
rameshpython <ramababugaganam5515@gmail.com>
2025-10-16 17:56:30 +0530

    loops star patterns number models

--- loopsstarpatterns.py
+++ loopsstarpatterns.py
@@ -60,3 +60,14 @@ for i in range(1, rows + 1):
 # Lower part
 for i in range(rows - 1, 0, -1):
     print(" " * (rows - i) + "*" * (2 * i - 1))
+
+print("----------------------------------------------------")
+
+rows=10
+for i in range(rows):
+    for j in range(rows):
+        print("*",end="")
+    print()
+
+rows=5
+for i in range()
