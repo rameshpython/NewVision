d944ca7c7e18b93b1f6c5968f4d300f48b8c3206
rameshpython <ramababugaganam5515@gmail.com>
2025-10-19 09:12:59 +0530

    starpattern loops with alphabets

--- loopsstarpatterns.py
+++ loopsstarpatterns.py
@@ -68,6 +68,3 @@ for i in range(rows):
     for j in range(rows):
         print("*",end="")
     print()
-
-rows=5
-for i in range()
--- loopsstarptrnnubr.py
+++ loopsstarptrnnubr.py
@@ -44,3 +44,75 @@ for i in range(1,rows+1):
         print(num,end="")
         num+=1
     print()
+print("-------------------------------")
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(65, 65 + i):     # 65 = ASCII value of 'A'
+        print(chr(j), end="")
+    print()
+
+print("---------------------------------")
+rows = 5
+for i in range(rows, 0, -1):
+    for j in range(65, 65 + i):
+        print(chr(j), end="")
+    print()
+
+print("--------------------------------")
+
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(chr(64 + i), end="")   # 64 + 1 = 65 → 'A'
+    print()
+
+print("----------------------------------")
+rows = 5
+ch = 65
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(chr(ch), end=" ")
+        ch += 1
+    print()
+print("-----------------------------------")
+rows = 5
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    for j in range(65, 65 + i):
+        print(chr(j), end="")
+    print()
+
+print("-----------------------------------")
+
+rows = 5
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    for j in range(65, 65 + i):
+        print(chr(j), end="")
+    for j in range(65 + i - 2, 64, -1):
+        print(chr(j), end="")
+    print()
+
+print("----------------------------------")
+rows = 5
+for i in range(rows, 0, -1):
+    print(" " * (rows - i), end="")
+    for j in range(65, 65 + i):
+        print(chr(j), end="")
+    for j in range(65 + i - 2, 64, -1):
+        print(chr(j), end="")
+    print()
+
+print("-----------------------------------")
+rows = 5
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+
+
+
+
+
