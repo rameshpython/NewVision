dbea6d1fb1e86406666b463c73bbc6cd926a62a5
rameshpython <ramababugaganam5515@gmail.com>
2025-10-20 11:59:24 +0530

    Arithmetic operators with question practice

--- Arithmeticoperators.py
+++ Arithmeticoperators.py
@@ -1,5 +1,5 @@
-a=9
-b=5
+a=10
+b=7
 print("Addition:",a+b)
 print("subtraction:",a-b)
 print("multiplication:",a*b)
@@ -9,9 +9,76 @@ print("Modulus:",a%b)
 print("exponent:",a**b)
 
 
-
+#Write a program to add two numbers entered by the user.
 a=float(input("Enter a value of a:"))
 b=float(input("Enter a value of b:"))
 print("\t Results of Arithmetic operators")
 print("\t{}+{}={}".format(a,b,a+b))
-5
+
+
+#Write a program to subtract two numbers.
+a=float(input("Enter a value of a:"))
+b=float(input("Enter a value of b:"))
+print("\t Results of subtract operators")
+print("\t{}+{}={}".format(a,b,a-b))
+
+
+#Write a program to multiply two numbers.
+a=float(input("Enter a value of a:"))
+b=float(input("Enter a value of b:"))
+print("\t Results of multiply operators")
+print("\t{}+{}={}".format(a,b,a*b))
+
+
+#Write a program to divide 50 by 6 using both / and //.
+a=50
+b=6
+print("Division:",a/b)
+print("Floor Division:",a//b)
+
+
+#Write a program to find the remainder when 25 is divided by 4.
+a=25
+b=4
+remainder=a%b
+print("The remainder when 25  is divided by 4 is:",remainder)
+
+print("----------------------------------------------------------")
+
+a=25
+b=4
+print("The modulus when 25 is divided by 4 is :",remainder)
+print("modulus:",a%b)
+
+
+#Write a program to find the square of a number.
+a= int(input("Enter a value of a:"))
+b=int(input("Enter a value of b:"))
+print("\t{}**{}={}".format(a,b,a**b))
+
+print("------------------------------------------------------------")
+
+num=float(input("enter a number:"))
+square=num**2
+print("The square of", num, "is:", square)
+
+
+#Write a program to find the cube of 7 using the ** operator.
+num=7
+cube=num**3
+print("The cube of", num, "is:", cube)
+
+a=7
+cube=a**3
+print("The cube of", num, "is", cube)
+
+
+#Add three numbers and find their average.
+a=8
+b=9
+c=4
+Average=a+b+c
+print("The average of three numbers")
+
+
+
---
+++ AverageOffThreeNumbers.py
@@ -0,0 +1,14 @@
+class AverageOffThreeNumbers:
+    def __init__(self):
+        print("Object created successfully!")
+
+    def average_off_three_numbers(self, number1, number2, number3):
+        average = (number1 + number2 + number3) / 3
+        print("The average of three numbers is:", average)
+
+
+# Create object
+demo = AverageOffThreeNumbers()
+
+# Call method
+demo.average_off_three_numbers(10, 20, 30)
