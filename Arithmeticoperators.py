36ce646af5503f038d336a34c8e1ed7e61b96f13
rameshpython <ramababugaganam5515@gmail.com>
2025-10-20 08:35:31 +0530

    operator with Arithmetics

---
+++ Arithmeticoperators.py
@@ -0,0 +1,17 @@
+a=9
+b=5
+print("Addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("Division:",a/b)
+print("floor Division:",a//b)
+print("Modulus:",a%b)
+print("exponent:",a**b)
+
+
+
+a=float(input("Enter a value of a:"))
+b=float(input("Enter a value of b:"))
+print("\t Results of Arithmetic operators")
+print("\t{}+{}={}".format(a,b,a+b))
+5
